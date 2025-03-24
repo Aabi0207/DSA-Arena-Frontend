090c002620dd026d8d364072fe81ae0f484bc500
Abhishek Biradar <abhishekbiradar0207@gmail.com>
2025-03-24 19:21:19 +0530

    changed the leaderboard images

--- src/components/LeaderboardRow/LeaderboardRow.jsx
+++ src/components/LeaderboardRow/LeaderboardRow.jsx
@@ -1,34 +1,34 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "./LeaderboardRow.css"
+import "./LeaderboardRow.css";
 
 const getRingClass = (rank) => {
-    if (rank === "Surya Bhai") return 'gold';
-    if (rank === "Rocky Bhai") return 'silver';
-    if (rank === "Bahubali") return 'bronze';
-    return '';
+  if (rank === "Surya Bhai") return "gold";
+  if (rank === "Rocky Bhai") return "silver";
+  if (rank === "Bahubali") return "bronze";
+  return "";
 };
 
 const getMedal = (pos) => {
-    if (pos == 1) return 'gold';
-    if (pos == 2) return 'silver';
-    if (pos == 3) return 'bronze';
-    return null;
-}
+  if (pos == 1) return "gold";
+  if (pos == 2) return "silver";
+  if (pos == 3) return "bronze";
+  return null;
+};
 
 const getRankImg = (rank) => {
-    if (rank == "Bahubali") return "/ranks/bahubali.png"
-    if (rank == "Rocky Bhai") return "/ranks/rocky.png"
-    if (rank == "Pushpa Bhau") return "/ranks/pushpa.png"
-    if (rank == "Mass") return "/ranks/mass.png"
-    if (rank == "Singham") return "/ranks/singham.png"
-    return "/ranks/chulbul.png"
-}
+  if (rank == "Bahubali") return "/ranks/bahubali.png";
+  if (rank == "Rocky Bhai") return "/ranks/rocky.png";
+  if (rank == "Pushpa Bhau") return "/ranks/pushpa.png";
+  if (rank == "Mass") return "/ranks/mass.png";
+  if (rank == "Singham") return "/ranks/singham.png";
+  return "/ranks/chulbul.png";
+};
 
 const LeaderboardRow = ({ pos, userSummary }) => {
-    const navigate = useNavigate();
-    const ringClass = getRingClass(userSummary.rank);
-    const medal = getMedal(pos)
+  const navigate = useNavigate();
+  const ringClass = getRingClass(userSummary.rank);
+  const medal = getMedal(pos);
 
   return (
     <>
@@ -37,26 +37,29 @@ const LeaderboardRow = ({ pos, userSummary }) => {
         <span className="comman username">{userSummary.username}</span>
         <span className="name">{userSummary.display_name}</span>
         <div className="comman sep-wrapper">
-        <div className={`wrapper ${ringClass}`}>
-          <img
-            src={`https://surya23.pythonanywhere.com/${userSummary.profile_photo}`}
-            alt="user-profile"
-            className="pfp"
-            onClick={() => {
-                navigate(`/profile/${userSummary.username}/`)
-            }}
-          />
-        </div>
+          <div className={`wrapper ${ringClass}`}>
+            <img
+              src={`https://surya23.pythonanywhere.com/${userSummary.profile_photo}`}
+              alt="user-profile"
+              className="pfp"
+              onClick={() => {
+                navigate(`/profile/${userSummary.username}/`);
+              }}
+            />
+          </div>
         </div>
 
         <span className="comman">{userSummary.score}</span>
         <div className="rank-img-contain">
-        <img src={getRankImg(userSummary.rank)} alt="Ranking imag" className="rank-img" />
-
+          <img
+            src={getRankImg(userSummary.rank)}
+            alt="Ranking imag"
+            className="rank-img"
+          />
         </div>
-          {medal && (
-            <img src={`/medals/${medal}.png`} alt="medal" className="medal" />
-          )}
+        {medal && (
+          <img src={`/medals/${medal}.png`} alt="medal" className="medal" />
+        )}
       </div>
     </>
   );
